5041b586ec2df92bd21dab0b7b42ab521a2bcf25
Banji <olabanji.ajayi@gmail.com>
2022-10-25 13:29:20 +0100

    continous move added

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-import { draw } from "./player1"
-import { update } from "./player1"
+
+import { update as updatePacBOY, draw as drawPacBOy } from "./pacBoy.js"
 
 let lastRenderTime = 0
 const GAME_SPEED = 2
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime){
     window.requestAnimationFrame(main)
@@ -13,82 +14,19 @@ function main(currentTime){
     console.log('Render')
 
 
-    // update()
-    // draw()
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
 
-// function update(){
+function update(){
+    updatePacBOY()
 
-// }
+}
 
-// function draw(){
+function draw(){
+    gameBoard.innerHTML =''
+    drawPacBOy(gameBoard)
 
-// }
-
-document.addEventListener('DOMContentLoaded',()=>{
-
-    const scoreDisplay = document.getElementById('score')
-    const width = 28
-    let score = 0
-    const grid = document.querySelector('.grid')
-    const layout = [
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-        4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
-        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
-        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
-        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
-        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-    ]
-    // 0 - pac-dots
-    // 1 - wall
-    // 2 - ghost-lair
-    // 3 - power-pellet
-    // 4 - empty
-
-    const squares = []
-
-    //create your board
-function createBoard() {
-    for (let i = 0; i < layout.length; i++) {
-        const square = document.createElement('div')
-        grid.appendChild(square)
-        squares.push(square)
-
-        //add layout to the board
-    if(layout[i] === 0) {
-        squares[i].classList.add('pac-dot')
-    } else if (layout[i] === 1) {
-        squares[i].classList.add('wall')
-    } else if (layout[i] === 2) {
-        squares[i].classList.add('ghost-lair')
-    } else if (layout[i] === 3) {
-        squares[i].classList.add('power-pellet')
-        }
-    }
-    }
-    createBoard()
-})
+}
--- pacBoy.js
+++ pacBoy.js
@@ -1,11 +1,25 @@
+import { getInputDirection } from "./input.js"
 
 
-const pacboy = [{x:11 ,y:11}]
+const pacboy = [{x:12 ,y:12}]
 
 export function update(){
+    const inputDirection = getInputDirection()
+    for (let i = pacboy.length-2; i >=0; i--){
+        pacboy[i+1]={...pacboy[i]}
+    }
+    pacboy[0].x += 1  //inputDirection
+    pacboy[0].y += 0 //inputDirection
     console.log('up')
 }
 
-export function draw(){
+export function draw(gameBoard){
+    pacboy.forEach(axies =>{
+        const pacElement = document.createElement('div')
+        pacElement.style.gridRowStart = axies.y
+        pacElement.style.gridColumnStart = axies.x
+        pacElement.classList.add('pacboy')
+        gameBoard.appendChild(pacElement)
+    })
     console.log('draw')
 }
